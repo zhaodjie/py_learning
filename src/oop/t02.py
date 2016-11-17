2a0388bcd5099fa9ff6ec577a67ab13586bb3b0a
zhaodj <dongjzhao@126.com>
2016-11-18 00:19:54 +0800

    注释掉对t01文件的引入

--- src/oop/t02.py
+++ src/oop/t02.py
@@ -3,7 +3,7 @@
 '''
  type()方法
 '''
-from oop.t01 import news
+#from oop.t01 import news
 
 print(type(1123))
 print(type(x for x in range(1,10)))  #生成器generator
@@ -12,4 +12,4 @@ print(type('str'))
 print(type(None)) # NoneType
 print(type(object)) #返回了 'type'
 print(type(abs)) #如果一个变量指向了函数或者类,也可用type()函数判断
-print(type(news)) #导入另一个py文件中的类，使用type()函数判断
+#print(type(news)) #导入另一个py文件中的类，使用type()函数判断
