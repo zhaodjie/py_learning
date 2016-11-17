7a3cdfdbbfcf28bdaeeb146565eb906c771bc623
zhaodj <dongjzhao@126.com>
2016-11-17 23:26:29 +0800

    type()函数的使用

--- src/oop/t01.py
+++ src/oop/t01.py
@@ -15,6 +15,36 @@ class News(object):
 
     def News_Info(self):
         print(self.__update_time,'\t', self.__title)
+    
+    #让外部代码通过类自己提供的方法，来访问属性 getter setter
+    def get_title(self):
+        return self.__title
+    def set_title(self,title):
+        self.__title = title
 
 news = News('上海已值深秋，今天飘起了小雨','2016-11-17 12:44:15')
 news.News_Info()
+#调用News类自己提供的方法，访问title属性
+title = '道狭草木长，夕露沾我衣，衣沾不足惜，但使愿无违'
+print('调用set方法：',news.set_title(title))
+print('调用get方法：',news.get_title())
+
+print('****************')
+#继承、多态
+class Animal(object):
+    def run(self):
+        print('父类的run方法ing')
+class Dog(Animal):
+    def __init__(self):
+        pass
+    def run(self):
+        print('dog类的run方法')
+dog = Dog()
+dog.run()
+ass = list()
+#判断实例对象 isinstance：list,set,dict,tuple,str,Object
+print(isinstance(ass, Animal))
+print(isinstance(news, News))
+#dog是Animal类型吗
+print(isinstance(dog, Animal))
+print(isinstance(dog, Dog))
---
+++ src/oop/t02.py
@@ -0,0 +1,15 @@
+# -*- coding:utf-8 -*-
+
+'''
+ type()方法
+'''
+from oop.t01 import news
+
+print(type(1123))
+print(type(x for x in range(1,10)))  #生成器generator
+print(type([1,44,15])) # list
+print(type('str'))
+print(type(None)) # NoneType
+print(type(object)) #返回了 'type'
+print(type(abs)) #如果一个变量指向了函数或者类,也可用type()函数判断
+print(type(news)) #导入另一个py文件中的类，使用type()函数判断
