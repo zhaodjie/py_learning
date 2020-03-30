939fa514514d136a3b2bafd47e307d1a518720b6
zhaodj <dongjzhao@126.com>
2020-03-30 18:32:23 +0800

    fetch all super link in specify websete

--- src/WebCatch/catchLink.py
+++ src/WebCatch/catchLink.py
@@ -1,23 +1,1 @@
-import requests
-import re
-
-url = "https://www.autohome.com.cn/shanghai/"
-urlBox = []
-def catchURL(url):
-    file = requests.get(url,timeout=2)
-    data = file.content
-    links = re.findall(r'(https?://[^\s)";]+\.(\w|/)*)',str(data))
-    print(links)
-    for i in links:
-        try:
-            currentURL = i
-            urlBox.append(currentURL)
-            #print(currentURL)
-            catchURL(currentURL)
-        except Exception as e:
-            pass
-        continue
-
-
-
-catchURL(url)
+import requestsimport reimport osurl = "https://www.autohome.com.cn/shanghai/"urlBox = []def catchURL(url):    file = requests.get(url,timeout=2)    data = file.content    links = re.findall(r'(https?://[^\s)";]+\.(\w|/)*)',str(data))    for i in links:        try:            currentURL = i[0]            if currentURL not in urlBox:                urlBox.append(currentURL)                os.system("ssh pgadmin@10.211.55.8 psql test -c \                            'insert into url values(nextval('url_seq'), '"+ currentURL +"')'")            print(currentURL)            catchURL(currentURL)        except Exception as e:            pass            continuecatchURL(url)
