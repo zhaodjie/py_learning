bd3dad98976d5e02c4a941ae3c687174db78781d
zhaodj <dongjzhao@126.com>
2020-03-31 10:25:53 +0800

    Put the crawled link into the database

--- src/WebCatch/catchLink.py
+++ src/WebCatch/catchLink.py
@@ -1,1 +1,32 @@
-import requestsimport reimport osurl = "https://www.autohome.com.cn/shanghai/"urlBox = []def catchURL(url):    file = requests.get(url,timeout=2)    data = file.content    links = re.findall(r'(https?://[^\s)";]+\.(\w|/)*)',str(data))    for i in links:        try:            currentURL = i[0]            if currentURL not in urlBox:                urlBox.append(currentURL)                os.system("ssh pgadmin@10.211.55.8 psql test -c \                            'insert into url values(nextval('url_seq'), '"+ currentURL +"')'")            print(currentURL)            catchURL(currentURL)        except Exception as e:            pass            continuecatchURL(url)
+import requests
+import re
+import os
+
+url = "https://www.autohome.com.cn/shanghai/"
+urlBox = []
+def catchURL(url):
+    file = requests.get(url,timeout=5)
+    data = file.content
+    links = re.findall(r'(https?://[^\s)";]+\.(\w|/)*)',str(data))
+    for i in links:
+        try:
+            currentURL = i[0]
+            if currentURL not in urlBox:
+                urlBox.append(currentURL)
+                sql = """
+                ssh pgadmin@10.211.55.8 psql test -U pgadmin << EOF
+                insert into url values(nextval(\'url_seq\'), \'"""+currentURL+"""\');
+                EOF
+                """
+                print(sql)
+                os.popen(sql)
+            print(currentURL)
+            catchURL(currentURL)
+        except Exception as e:
+            pass
+            continue
+
+
+
+
+catchURL(url)
